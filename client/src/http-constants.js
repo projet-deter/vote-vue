17a5fa6caef81b897484cc77faaa2d2815a50cbc
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2020-01-23 00:25:23 +0100

    Ajout du module Axios et des constantes de config env

---
+++ client/src/http-constants.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+let baseURL;
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:8081/";
+} else {
+  baseURL = "http://localhost:8081/";
+}
+
+export const HTTP = axios.create({
+  baseURL: baseURL
+});
