10ff638723964b8b376b96ee860a0d7583bc84d1
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2020-01-25 14:06:27 +0100

    Create first components and routing system

---
+++ client/src/components/Page/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Connexion</h1>
+    <button>Log in</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style scoped>
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import router from './routes/router';
+// import store from './vuex/store';
+
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  // store,
   render: h => h(App),
 }).$mount('#app')
